6d6c88d135433356453063432a4665c8c44687a2
hide10 <hide10@gmail.com>
2020-09-06 17:40:59 +0900

    differenceの練習課題

---
+++ difference.py
@@ -0,0 +1,11 @@
+# difference
+
+store1 = {7, 9, 2}
+store2 = {18, 22, 3, 7, 12, 9}
+print(store1. difference(store2))
+print(store2. difference(store1))
+store3 = {11, 7, 3, 6, 9, 15}
+print(store2.difference(store1, store3))
+
+print(store1.symmetric_difference(store2))
+print(store2.symmetric_difference(store1))
